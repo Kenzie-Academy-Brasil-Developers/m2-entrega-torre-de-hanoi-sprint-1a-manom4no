20a8343d670db508966943b518136160d009f33a
andersonStorqui <andersoncxszn@gmail.com>
2021-11-30 14:34:54 -0300

    tentando

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,42 @@
+//container
+
+let towerContainer = document.createElement('div');
+towerContainer.setAttribute('class', 'towerContainer');
+
+//colunas
+
+let towerRodZero = document.createElement('ul');
+towerRodZero.setAttribute('class', 'towerRodZero');
+towerRodZero.setAttribute('id','coluna1');
+towerRodZero.addEventListener('click', firstButton);
+let towerRodOne = document.createElement('ul');
+towerRodOne.setAttribute('class', 'towerRodOne');
+towerRodOne.setAttribute('id', 'coluna2');
+towerRodOne.addEventListener('click', firstButton);
+let towerRodTwo = document.createElement('ul');
+towerRodTwo.setAttribute('class', 'towerRodTwo');
+towerRodTwo.setAttribute('id', 'coluna3');
+towerRodTwo.addEventListener('click',firstButton);
+
+//linhas
+
+let liZero = document.createElement('li');
+liZero.setAttribute('class','liZero');
+liZero.setAttribute('id','disco1');
+let liOne = document.createElement('li');
+liOne.setAttribute('class','liOne');
+liOne.setAttribute('id','disco2');
+let liTwo = document.createElement('li');
+liTwo.setAttribute('class','liTwo');
+liTwo.setAttribute('id','disco3');
+let liThree = document.createElement('li');
+liThree.setAttribute('class','liThree');
+liThree.setAttribute('id','disco4');
+
+
+
 function towerCreation (){
-    //container
 
-    let towerContainer = document.createElement('div');
-    towerContainer.setAttribute('class', 'towerContainer');
-
-    //colunas
-
-    let towerRodZero = document.createElement('ul');
-    towerRodZero.setAttribute('class', 'towerRodZero');
-    towerRodZero.setAttribute('id','coluna1')
-    let towerRodOne = document.createElement('ul');
-    towerRodOne.setAttribute('class', 'towerRodOne');
-    towerRodOne.setAttribute('id', 'coluna2')
-    let towerRodTwo = document.createElement('ul');
-    towerRodTwo.setAttribute('class', 'towerRodTwo');
-    towerRodTwo.setAttribute('id', 'coluna3')
-
-    //linhas
-
-    let liZero = document.createElement('li');
-    liZero.setAttribute('class','liZero');
-    let liOne = document.createElement('li');
-    liOne.setAttribute('class','liOne');
-    let liTwo = document.createElement('li');
-    liTwo.setAttribute('class','liTwo');
-    let liThree = document.createElement('li');
-    liThree.setAttribute('class','liThree');
 
     //estrutura
     towerRodZero.appendChild(liZero);
@@ -41,21 +52,13 @@ function towerCreation (){
 }
 towerCreation()
 
-function firstButton(){
-    
-}
-function secondButton(){
-    
-}
-function thirdButton(){
-    
-}
-function fourthButton(){
-    
-}
-function fifthButton(){
-    
-}
-function sixthButton(){
-    
+
+let lastButton = towerRodZero.lastElementChild;
+let lastButtonOne = towerRodOne.lastElementChild;
+let lastButtonTwo = towerRodTwo.lastElementChild;
+
+
+function firstButton(event){
+    console.log(event.target)
+    console.log(event.currentTarget)
 }
--- scriptLogica.js
+++ scriptLogica.js
@@ -1,24 +1,22 @@
-/*
-criar condição para pegar sempre o menor disco
-    usando classe para verificar com a de menor tamanho
-*/
+// // PEGANDO ID
+// let coluna1 = document.getElementById('coluna1');
+// let coluna2 = document.getElementById('coluna2');
+// let coluna3 = document.getElementById('coluna3');
 
-function pegarSempreMenorDisco (){
-    let towerRodZero = document.getElementById('coluna1')
-    let towerRodOne = document.getElementById('coluna2')
-    let towerRodTwo = document.getElementById('coluna3')
-    
-    towerRodZero.lastElementChild
-    
-    
-// usar lastChildElements para pegar sempre o ultimo da div
-}
-pegarSempreMenorDisco()
+// // PEGAR TAMANHOS DOS DISCOS
+// let disco1 = document.getElementById('disco1').clientWidth;
+// let disco2 = document.getElementById('disco2').clientWidth;
+// let disco3 = document.getElementById('disco3').clientWidth;
+// let disco4 = document.getElementById('disco4').clientWidth;
 
+// function adicionandoEventoDeCliqueNaVareta (){
+//     // FAZER VERIFICAÇÃO
+//     // SE O DISCO FOR MENOR AI FAZ UM APPENDCHILD
+//     // SE TIVER VAZIO, NENHUM ELEMENTO FILHO, DAR APPENDCHILD TAMBEM
+//     // SE O DISCO FOR MAIOR QUE O QUE JÁ TEM NA TORRE, MOSTRAR ERRO
+//     //E NÃO MOVIMENTAR O DISCO
 
-
-
-
+//     coluna1.addEventListener('')
 
 
 
@@ -27,33 +25,81 @@ pegarSempreMenorDisco()
 
 
 
+//     let discoTopoColuna1 = coluna1.lastElementChild;
+//     let discoTopoColuna2 = coluna2.lastElementChild;
+//     let discoTopoColuna3 = coluna3.lastElementChild;
+
+
+//     if(coluna1.children[0] === undefined){
+
+//     }
+//     if(coluna2.children[0] === undefined){
+
+//     }
+//     if(coluna3.children[0] === undefined){
+
+//     }
 
 
 
 
 
 
+//     //pegar um disco que tem uma vareta quando clicar
+//     //depois clicar em outra vareta
+//     //ai fazer uma verificação se o primeiro disco que clicou
+//     //é menor do que o disco da vareta que pretende jogar
 
 
 
 
+//     //pegar evento de clique da vareta
+//     //fazer if se o disco é menor do  que o que ja tem na vareta
+//     //ai move ele, da appendchild
 
-
-
-// let liZero = document.querySelector('liZero');
-// let liOne = document.querySelector('liOne');
-// let liTwo = document.querySelector('liTwo');
-// let liThree = document.querySelector('liThree');
-
-// liZero = element.clientWidth;
-// liOne = element.clientWidth;
-// liTwo = element.clientWidth;
-// liThree = element.clientWidth;
-// 
-// if(liTwo < liThree){
-    // pegar liTwho e passar para outra vareta
-// }else if(liOne < liTwo){
-    // pegar liOne e passar para outra vareta
-// }else if(liZero < liOne){
-    // pegar liZero e passar para outra vareta
+//     //dois evento para a torre que quer mudar o disco
+//     //e o outro evento para a torre que vai
+// // usar lastChildElements para pegar sempre o ultimo da div
 // }
+// pegarSempreMenorDisco()
+
+
+
+
+// //passar addeventListener na ul
+// //interceptar apenas os botões
+// function interceptandoEventos (){
+//     let buttonContainer = document.querySelector('.buttonContainer');
+//     buttonContainer.addEventListener('click', function(evt){
+//         console.log(evt.target)
+//         console.log(evt.currentTarget)
+//     });
+// }
+
+
+
+
+
+
+
+
+
+
+// interceptandoEventos()
+
+
+
+
+
+
+
+
+
+// // 
+// // if(liTwo < liThree){
+//     // pegar liTwho e passar para outra vareta
+// // }else if(liOne < liTwo){
+//     // pegar liOne e passar para outra vareta
+// // }else if(liZero < liOne){
+//     // pegar liZero e passar para outra vareta
+// // }
