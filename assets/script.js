b2656ebe21ab0733b7ef7723c7a1e693173be910
Luan-Dev <luanpbs98@gmail.com>
2021-12-01 00:04:55 -0300

    finalizado_sem_bonus

--- assets/script.js
+++ assets/script.js
@@ -2,36 +2,41 @@
 
 let towerContainer = document.createElement('div');
 towerContainer.setAttribute('class', 'towerContainer');
+towerContainer.setAttribute('id','towerContainer')
 
 //colunas
 
 let towerRodZero = document.createElement('ul');
 towerRodZero.setAttribute('class', 'towerRodZero');
 towerRodZero.setAttribute('id','coluna1');
-towerRodZero.addEventListener('click', takeDisc);
+towerRodZero.addEventListener('click', deployDisc);
 let towerRodOne = document.createElement('ul');
 towerRodOne.setAttribute('class', 'towerRodOne');
 towerRodOne.setAttribute('id', 'coluna2');
-towerRodOne.addEventListener('click', takeDisc);
+towerRodOne.addEventListener('click', deployDisc);
 let towerRodTwo = document.createElement('ul');
 towerRodTwo.setAttribute('class', 'towerRodTwo');
 towerRodTwo.setAttribute('id', 'coluna3');
-towerRodTwo.addEventListener('click',takeDisc);
+towerRodTwo.addEventListener('click',deployDisc);
 
 //linhas
 
 let liZero = document.createElement('li');
 liZero.setAttribute('class','liZero');
 liZero.setAttribute('id','disco1');
+liZero.addEventListener('click', takeDisc);
 let liOne = document.createElement('li');
 liOne.setAttribute('class','liOne');
 liOne.setAttribute('id','disco2');
+liOne.addEventListener('click', takeDisc);
 let liTwo = document.createElement('li');
 liTwo.setAttribute('class','liTwo');
 liTwo.setAttribute('id','disco3');
+liTwo.addEventListener('click', takeDisc);
 let liThree = document.createElement('li');
 liThree.setAttribute('class','liThree');
 liThree.setAttribute('id','disco4');
+liThree.addEventListener('click', takeDisc);
 
 
 
@@ -47,22 +52,25 @@ function towerCreation (){
 }
 towerCreation()
 
-let count = 0
-
+let count = 0;
+let take;
 function takeDisc(evt){
-    const addDisc = evt.currentTarget;
-    let take = addDisc.lastElementChild;
-
-    function deployDisc(evt){
-        const compactDisc = evt.currentTarget
-
-        if(takeDisc.clientWidth < compactDic.lastElementChild.clientWidth){
-            evt.appenchild(take[0])
-            count++
-        }else{
-            alert("não pode por um disco maior por cima do menor")
-        }
-    }
+    let add = evt.currentTarget.closest('ul')
+    take = add.lastElementChild
 }
 
-/*condição de vitoria*/
+function deployDisc(evt){
+    const repositoryDisc = evt.currentTarget.closest('ul')
+    if(repositoryDisc.childElementCount === 0){
+        repositoryDisc.appendChild(take)
+    }
+    if(repositoryDisc.lastElementChild.clientWidth > take.clientWidth){
+        repositoryDisc.appendChild(take)
+    }
+    if(repositoryDisc.lastElementChild.clientWidth < take.clientWidth){
+        alert('nao pode colocar o maior sobre o menor')
+    }
+    if(towerRodZero.childElementCount === 0 && towerRodOne.childElementCount === 0){
+        document.getElementById('footer').style.display='flex';
+    }
+}
